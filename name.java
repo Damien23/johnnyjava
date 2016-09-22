ecb9afd9031b28523a5efce364475194cddc1d5b
Damien Clancy <t00193753@365s.ittralee.ie>
2016-09-22 10:49:44 +0100

    Original files

---
+++ name.java
@@ -0,0 +1,11 @@
+//Name.java
+/*This will display my name*/
+
+public class name
+ {
+public static void main(String args[])
+{
+	System.out.println("My name is Damien Clancy. Today is 22/09/2016. Power on!");
+}	
+}
+
